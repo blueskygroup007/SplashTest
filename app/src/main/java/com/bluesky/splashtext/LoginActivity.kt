43eb2463dd99dd4a295049ce937444d763b7f7d7
bluesky <16733682@qq.com>
2023-08-21 00:57:38 +0800

    初时基本完成

--- app/src/main/java/com/bluesky/splashtext/LoginActivity.kt
+++ app/src/main/java/com/bluesky/splashtext/LoginActivity.kt
@@ -1,19 +1,28 @@
 package com.bluesky.splashtext
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -30,19 +39,42 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
     }
 
+    @Composable
+    fun Conversation(message: List<Message>) {
+        LazyColumn {
+            items(message) {
+                MessageCard(msg = it)
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewConversation() {
+        SplashTestTheme {
+            Conversation(message = MsgData.messages)
+        }
+    }
 
     @Composable
     fun MessageCard(msg: Message) {
+
+        var isExpanded by remember { mutableStateOf(false) }
+
         Surface(
             shape = MaterialTheme.shapes.medium,
-            modifier = Modifier.padding(all = 8.dp),
-            tonalElevation = 5.dp
+            tonalElevation = 5.dp,
+            modifier = Modifier
+                .padding(all = 8.dp)
+                .padding(all = 8.dp)
+                .clickable { isExpanded = !isExpanded }
+
         ) {
             Row(
                 modifier = Modifier.padding(all = 8.dp)
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.girl),
+                    painter = painterResource(id = R.drawable.girl_robot_avatar),
                     contentDescription = "profile picture",
                     modifier = Modifier
                         .size(50.dp)
@@ -63,7 +95,9 @@ class LoginActivity : AppCompatActivity() {
                     Spacer(modifier = Modifier.padding(vertical = 4.dp))
                     Text(
                         text = msg.body,
-                        style = MaterialTheme.typography.bodyMedium
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        modifier = Modifier.animateContentSize()
                     )
                 }
             }
@@ -73,7 +107,12 @@ class LoginActivity : AppCompatActivity() {
     }
 
 
-    @Preview
+    @Preview(name = "light mode")
+    @Preview(
+        name = "dark mode",
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true
+    )
     @Composable
     fun PreviewMessageCard() {
         SplashTestTheme {
